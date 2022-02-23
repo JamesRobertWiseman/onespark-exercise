86b3389d221345ec80b6df72c53d2ecb2367019a
Burst Design <62024516+JamesRobertWiseman@users.noreply.github.com>
2022-02-23 19:04:13 +0000

    Add meta description and html lang tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 const Document: React.FC = () => (
-  <Html>
+  <Html lang="en">
     <Head>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Khula:wght@300;400;600;700;800&family=Nanum+Pen+Script&display=swap" rel="stylesheet"></link>
+        <meta name="description" content="A single page application build for OneSpark to demonstrate my skills."/>
     </Head>
     <body>
       <Main />
